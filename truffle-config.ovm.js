f1381effa012308958bc8238e7c9e06b8531c8f4
korrio <iamnotkorr@gmail.com>
2021-07-28 00:27:55 +0700

    can't verify contracts on optimism kovan due to no solc on etherscan match exactly with solc for optimism and the truffle-plugin-verify is not support on network 69 (optimism kovan)

--- truffle-config.ovm.js
+++ truffle-config.ovm.js
@@ -7,6 +7,7 @@ const kovanMnemonic = process.env["KOVAN_MNEMONIC"];
 const mnemonic = process.env["MNEMONIC"];
 
 const infuraKey = process.env["INFURA_KEY"];
+const etherApiKey = process.env["ETHERSCAN_KEY"];
 
 //uncomment to use mainnetMnemonic, be sure to set it in the .env file
 //const mainnetMnemonic = process.env["MAINNET_MNEMONIC"]
@@ -16,85 +17,92 @@ const HDWalletProvider = require('@truffle/hdwallet-provider');
 
 module.exports = {
 
-  /**
-  * contracts_build_directory tells Truffle where to store compiled contracts
-  */
-  contracts_build_directory: './build/optimism-contracts',
+    /**
+     * contracts_build_directory tells Truffle where to store compiled contracts
+     */
+    contracts_build_directory: './build/optimism-contracts',
 
-  /**
-  *  contracts_directory tells Truffle where to find your contracts
-  */
-  contracts_directory: './contracts/optimism',
+    /**
+     *  contracts_directory tells Truffle where to find your contracts
+     */
+    contracts_directory: './contracts/optimism',
 
-  networks: {
-    development: {
-      url: "http://127.0.0.1:7545",
-      network_id: "*",
-    },
-    ganache: {
-      network_id: 108,
-      networkCheckTimeout: 100000,
-      provider: function() {
-        return ganache.provider({
-          mnemonic: ganacheMnemonic,
-          network_id: 108,
-          default_balance_ether: 100,
-        })
-      }
-    },
-    // for use with local environment -- see README and list of available
-    // scripts in package.json for steps to get this running on your local machine
-    optimistic_ethereum: {
-      network_id: 420,
-      gas:  15000000,
-      provider: function() {
-        return new HDWalletProvider({
-          mnemonic: {
-            phrase: mnemonic
-          },
-          providerOrUrl: "http://127.0.0.1:8545/",
-          addressIndex: 0,
-          numberOfAddresses: 1,
-          chainId: 420
-        })
-      }
-    },
-    optimistic_kovan: {
-      network_id: 69,
-      chain_id: 69,
-      // gas: 15970000,
-      gas: 141010000,
-      provider: function() {
-        return new HDWalletProvider(kovanMnemonic, "https://optimism-kovan.infura.io/v3/"+ infuraKey, 0, 1);
-      }
-    },
-    // requires a mainnet mnemonic; you can save this in .env or in whatever secure location
-    // you wish to use
-    optimistic_mainnet: {
-      network_id: 10,
-      chain_id: 10,
-      provider: function() {
-        return new HDWalletProvider(mainnetMnemonic, "https://optimism-mainnet.infura.io/v3/" + infuraKey, 0, 1);
-      }
-    }
-
-  },
-
-  mocha: {
-    timeout: 100000
-  },
-  compilers: {
-    solc: {
-      version: "node_modules/@eth-optimism/solc",
-      settings:  {
-        optimizer: {
-          enabled: true,
-          runs: 800
+    networks: {
+        development: {
+            url: "http://127.0.0.1:7545",
+            network_id: "*",
+        },
+        ganache: {
+            network_id: 108,
+            networkCheckTimeout: 100000,
+            provider: function() {
+                return ganache.provider({
+                    mnemonic: ganacheMnemonic,
+                    network_id: 108,
+                    default_balance_ether: 100,
+                })
+            }
+        },
+        // for use with local environment -- see README and list of available
+        // scripts in package.json for steps to get this running on your local machine
+        optimistic_ethereum: {
+            network_id: 420,
+            gas: 15000000,
+            provider: function() {
+                return new HDWalletProvider({
+                    mnemonic: {
+                        phrase: mnemonic
+                    },
+                    providerOrUrl: "http://127.0.0.1:8545/",
+                    addressIndex: 0,
+                    numberOfAddresses: 1,
+                    chainId: 420
+                })
+            }
+        },
+        optimistic_kovan: {
+            network_id: 69,
+            chain_id: 69,
+            // gas: 15970000,
+            gas: 141010000,
+            provider: function() {
+                return new HDWalletProvider(kovanMnemonic, "https://optimism-kovan.infura.io/v3/" + infuraKey, 0, 1);
+            }
+        },
+        // requires a mainnet mnemonic; you can save this in .env or in whatever secure location
+        // you wish to use
+        optimistic_mainnet: {
+            network_id: 10,
+            chain_id: 10,
+            provider: function() {
+                return new HDWalletProvider(mainnetMnemonic, "https://optimism-mainnet.infura.io/v3/" + infuraKey, 0, 1);
+            }
         }
-      }
+
     },
-  },
-  db: {
-    enabled: false
-  }
+
+    mocha: {
+        timeout: 100000
+    },
+    compilers: {
+        solc: {
+            version: "node_modules/@eth-optimism/solc",
+            settings: {
+                optimizer: {
+                    enabled: true,
+                    runs: 800
+                }
+            }
+        },
+    },
+    plugins: [
+        'truffle-plugin-verify'
+    ],
+    api_keys: {
+        etherscan: etherApiKey,
+        // bscscan: bscApiKey,
+    },
+    db: {
+        enabled: false
+    }
 }
